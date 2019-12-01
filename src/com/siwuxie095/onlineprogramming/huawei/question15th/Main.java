1d4f8478b62764e0f2eaa329917f48e7a5ea2e53
siwuxie095 <834879583@qq.com>
2019-12-01 14:54:52 +0800

    求 int 型正整数在内存中存储时 1 的个数；

---
+++ src/com/siwuxie095/onlineprogramming/huawei/question15th/Main.java
@@ -0,0 +1,45 @@
+package com.siwuxie095.onlineprogramming.huawei.question15th;
+
+import java.util.Scanner;
+
+/**
+ * 求int型正整数在内存中存储时1的个数
+ *
+ * 题目描述
+ * 输入一个int型的正整数，计算出该int型数据在内存中存储时1的个数。
+ *
+ * 输入描述:
+ *  输入一个整数（int类型）
+ *
+ * 输出描述:
+ * 这个数转换成2进制后，输出1的个数
+ *
+ *
+ * 示例1
+ *
+ * 输入
+ * 5
+ *
+ * 输出
+ * 2
+ *
+ * @author Jiajing Li
+ * @date 2019-12-01 14:44:34
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int val = scanner.nextInt();
+        int count = 0;
+        while (val != 0) {
+            int remainder = val % 2;
+            val = val / 2;
+            if (remainder == 1) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+}
