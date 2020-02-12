574b980056a77d591658040f1c7d40541486cdc1
siwuxie095 <834879583@qq.com>
2020-02-12 18:35:13 +0800

    判断两个IP是否属于同一子网；

---
+++ src/com/siwuxie095/onlineprogramming/huawei/question039/Main.java
@@ -0,0 +1,15 @@
+package com.siwuxie095.onlineprogramming.huawei.question039;
+
+/**
+ * 判断两个IP是否属于同一子网（题目为空）
+ *
+ * @author Jiajing Li
+ * @date 2020-02-12 18:33:51
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
