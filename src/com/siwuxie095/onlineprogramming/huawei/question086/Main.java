77ccffab3b4e220553805e2089dcbc4826ce36e8
jj.li <834879583@qq.com>
2020-05-15 20:34:24 +0800

    求最大连续bit数；

---
+++ src/com/siwuxie095/onlineprogramming/huawei/question086/Main.java
@@ -0,0 +1,61 @@
+package com.siwuxie095.onlineprogramming.huawei.question086;
+
+import java.util.Scanner;
+
+/**
+ * 求最大连续bit数
+ *
+ * 题目描述
+ * 功能: 求一个byte数字对应的二进制数字中1的最大连续数，例如3的二进制为00000011，最大连续2个1
+ * 输入: 一个byte型的数字
+ * 输出: 无
+ * 返回: 对应的二进制数字中1的最大连续数
+ *
+ * 输入描述:
+ * 输入一个byte数字
+ *
+ * 输出描述:
+ * 输出转成二进制之后连续1的个数
+ *
+ *
+ * 示例1
+ *
+ * 输入
+ * 3
+ *
+ * 输出
+ * 2
+ *
+ * @author Jiajing Li
+ * @date 2020-05-15 17:45:13
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int val = scanner.nextInt();
+            int serialCount = 0;
+            int resettableCount = 0;
+            boolean flag = false;
+            while (val != 0) {
+                int remainder = val % 2;
+                val = val / 2;
+                if (remainder == 1) {
+                    if (!flag) {
+                        flag = true;
+                    }
+                    resettableCount++;
+                    serialCount = Math.max(serialCount, resettableCount);
+                } else {
+                    flag = false;
+                    resettableCount = 0;
+                }
+            }
+            System.out.println(serialCount);
+        }
+    }
+
+
+}
