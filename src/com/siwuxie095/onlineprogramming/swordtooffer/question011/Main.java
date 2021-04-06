031a1188e5b0610fdcd139e096a532f52ce9b11b
siwuxie095 <834879583@qq.com>
2021-04-06 10:46:51 +0800

    JZ11 二进制中 1 的个数；

--- src/com/siwuxie095/onlineprogramming/swordtooffer/question011/Main.java
+++ src/com/siwuxie095/onlineprogramming/swordtooffer/question011/Main.java
@@ -1,8 +1,8 @@
-package com.siwuxie095.onlineprogramming.swordtooffer.question010;
+package com.siwuxie095.onlineprogramming.swordtooffer.question011;
 
 /**
  * @author Jiajing Li
- * @date 2021-04-02 16:27:18
+ * @date 2021-04-06 10:47:12
  */
 @SuppressWarnings("all")
 public class Main {
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + " - " + solution.rectCover(i));
+            System.out.println(i + " - " + solution.NumberOf1(i));
         }
     }
 
