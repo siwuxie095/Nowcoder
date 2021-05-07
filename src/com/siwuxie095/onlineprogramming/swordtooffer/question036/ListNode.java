dd4885b7f2504a8cc325de52ddf9710636a86dbc
siwuxie095 <834879583@qq.com>
2021-05-07 19:48:28 +0800

    JZ36 两个链表的第一个公共结点；

---
+++ src/com/siwuxie095/onlineprogramming/swordtooffer/question036/ListNode.java
@@ -0,0 +1,26 @@
+package com.siwuxie095.onlineprogramming.swordtooffer.question036;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-05-07 19:35:53
+ */
+@SuppressWarnings("all")
+public class ListNode {
+
+    public int val;
+
+    public ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+
+}
---
+++ src/com/siwuxie095/onlineprogramming/swordtooffer/question036/Main.java
@@ -0,0 +1,10 @@
+package com.siwuxie095.onlineprogramming.swordtooffer.question036;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-05-07 19:35:02
+ */
+@SuppressWarnings("all")
+public class Main {
+
+}
